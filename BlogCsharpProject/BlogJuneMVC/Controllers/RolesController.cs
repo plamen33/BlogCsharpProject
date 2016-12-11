eb5eb9f76a7c2b51f71e75530feef34068b66c30
Plamen <mail@m.com>
2016-12-11 20:00:20 +0200

    Implementation of Roles Extension functionality - Identity Roles are operational from GUI

---
+++ BlogCsharpProject/BlogJuneMVC/Controllers/RolesController.cs
@@ -0,0 +1,110 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BlogJuneMVC.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.Owin;
+using BlogJuneMVC.Extensions;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+
+namespace BlogJuneMVC.Controllers
+{
+    [Authorize(Roles = "Administrators")]
+    public class RolesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            var roles = db.Roles.ToList();
+            return View(roles);
+        }
+
+        // GET: /Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Roles/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                db.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole()
+                {
+                    Name = collection["RoleName"]
+                });
+                db.SaveChanges();
+                ViewBag.ResultMessage = "Role created successfully !";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(string RoleName)
+        {
+            var thisRole = db.Roles.Where(r => r.Name.Equals(RoleName, System.StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            db.Roles.Remove(thisRole);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Roles/Edit/5
+        public ActionResult Edit(string roleName)
+        {
+            var thisRole = db.Roles.Where(r => r.Name.Equals(roleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+            return View(thisRole);
+        }
+
+        //
+        // POST: /Roles/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Microsoft.AspNet.Identity.EntityFramework.IdentityRole role)
+        {
+            try
+            {
+                db.Entry(role).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
+
+
+//using BlogJuneMVC.Models;
+//using MVCBlog.Models;
+
+//namespace BlogJuneMVC.Controllers
+
+//Post post = db.Posts.Include(p => p.Author).SingleOrDefault(p => p.Id == id);
+//            if (post == null || post.Author == null || post.Author.UserName != User.Identity.Name)
